c6cc6e0bc96ceb67feabf77c5132a1f3b630e2f6
kunal3 <kagrawa@purdue.edu>
2015-12-09 18:09:32 -0500

    still trying

--- server.py
+++ server.py
@@ -10,9 +10,9 @@ except ImportError:
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
-execfile('requests/setup.py')
+#execfile('requests/setup.py')
 #execfile('python-firebase-1.2/setup.py')
-#execfile('glitchify.py')
+execfile('glitchify.py')
 httpd = Server(("", PORT), Handler)
 try:
   print("Start serving at port %i" % PORT)
