94d248fd663e23e833c79d2b5ababdae7603d82c
kunal3 <kagrawa@purdue.edu>
2015-12-10 07:28:02 -0500

    took out key in post call

--- static/glitchify.py
+++ static/glitchify.py
@@ -50,7 +50,7 @@ def main():
 					while(data['toReplace'] in image[random.randint(0,36)]):
 						data['toReplace'] = image[random.randint(36, data['filesize'] - 8)]
 					key = uuid.uuid1()
-					directOutput = subprocess.check_output("curl -X POST -d \'{\""+str(key)+"\":"+json.dumps(data)+"}\' https://glitchify.firebaseio.com/images.json",shell = True)
+					directOutput = subprocess.check_output("curl -X POST -d \'"+json.dumps(data)+"\' https://glitchify.firebaseio.com/images.json",shell = True)
 					#os.system("curl -X POST -d \'{\""+str(key)+"\":"+json.dumps(data)+"}\' https://glitchify.firebaseio.com/images.json")
 					glitched = glitchFuncs[ data['func'] ](image, data)
 					f = open('imgOut/'+data['filename'], "wb")
