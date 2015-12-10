2e94ed9f7e82dfd1f072fe67500dbfa85070e130
kunal3 <kagrawa@purdue.edu>
2015-12-10 00:23:00 -0500

    Added test for echo

--- static/glitchify.py
+++ static/glitchify.py
@@ -112,6 +112,7 @@ def echo(image, data):
 	for i in range(36, data['filesize']-4):
 		#s = hex(int(image[i],16)/2 + int(imageCopy[data['filesize'] - i],16)/2)[2:] 
 		s = "00"
+		a = hex(90) 
 		if len(s)%2:
 			s = "0"+s
 		image[i] = s
